70576decc791a035d7a4c010b36aa91940b3a005
Carlos Valladares <115051589+vgcarlol@users.noreply.github.com>
2024-05-22 01:34:24 -0600

    Create webpack.config.js

---
+++ .github/workflows/webpack.config.js
@@ -0,0 +1,14 @@
+module: {
+    rules: [
+        {
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-react']
+                }
+            }
+        }
+    ]
+}
